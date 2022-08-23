ca3deb531230eb77cbc4e50861951f2fde3b1d44
onesmus <onesmusqamara@gmail.com>
2022-08-23 09:47:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 9;
+let num4 = 5;
+console.log(multiply)
+
+
+
+
+const random = Math.floor (Math.random()* 10) + 1;
+const multiply = `num1*num2`;
+const mod = `num3 % num4`;
+const max = Math.max(1, 2, 3, 4, 20,);
+
